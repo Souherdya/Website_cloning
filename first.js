5dfd3743b70d361fba9589018c48c8e0a39b10e6
Souherdya <152802753+Souherdya@users.noreply.github.com>
2024-01-25 17:12:36 +0530

    Delete first.js

--- first.js
+++
@@ -1,9 +0,0 @@
-
-function email()
-{
-let Email_id = document.getElementById("Email_id").value;
-let Password = document.getElementById("Password").value;
-
-console.log("Id: ",Email_id ,"\n","Pass: ", Password );
-
-}
