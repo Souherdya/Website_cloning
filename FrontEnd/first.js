f8c199c0298031fc0ec77bf045d5481596f0c285
Souherdya <152802753+Souherdya@users.noreply.github.com>
2024-01-25 17:11:26 +0530

    Add files via upload

---
+++ FrontEnd/first.js
@@ -0,0 +1,9 @@
+
+function email()
+{
+let Email_id = document.getElementById("Email_id").value;
+let Password = document.getElementById("Password").value;
+
+console.log("Id: ",Email_id ,"\n","Pass: ", Password );
+
+}
